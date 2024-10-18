2f07e13823382e6bc9f2a94d6a71ffb54b74308f
Wilson Magalhães <wilson.magalhaes@gmail.com>
2024-10-18 00:27:39 -0300

    Altercao na parte da tabela de ips e no requirements.txt

--- main.py
+++ main.py
@@ -6,41 +6,59 @@ app,rt = fast_app(live=True)
 
 @rt('/')
 def get(): 
-  return Body(
-                Header(
-                  H1("Teste")
-                ),                
-                Div(
-                 Table(
-                   Thead(
-                     Th('VLAN'),
-                     Th('NETWORKING'),
-                     Th('IP'),
-                     Th('STATUS'),                     
-                   ),
-                   Tbody(
-                     Tr(
-                       Th("505"),
-                       Td("10.236.5.0/24"),
-                       Td("10.236.5.29"),
-                       Td("ok"),                      
-                     ),
-                     Tr(
-                       Th("505"),
-                       Td("10.236.5.0/24"),
-                       Td("10.236.5.30"),
-                       Td("ok"),                      
-                     ),
-                     Tr(
-                       Th("505"),
-                       Td("10.236.5.0/24"),
-                       Td("10.236.5.31"),
-                       Td("ok"),                      
-                     ),
-                     
-                   )
-                 )
-                ),  
+  return Titled("Manager IPs", 
+                
+                Body(
+                  Header(
+                    
+                      H2("Tabela de ips"),
+                      Search(
+                        Input(type="search"),
+                        Button("Search"),
+                        target_id="ip-tables",
+                        hx_select="ip-tables",
+                      )
+                                      
+
+                  ),
+                  Div(
+                    Table(
+                      Thead(
+                        Th('VLAN'),
+                        Th('NETWORKING'),
+                        Th('IP'),
+                        Th('NOME SERVIDOR'),
+                        Th('STATUS'),                     
+                      ),
+                      Tbody(
+                        Tr(
+                          Th("505"),
+                          Td("10.236.5.0/24"),
+                          Td("10.236.5.29"),
+                          Td("VSH-NGINX-SERVER"),
+                          Td("ok"),                      
+                        ),
+                            
+                        Tr(
+                          Th("505"),
+                          Td("10.236.5.0/24"),
+                          Td("10.236.5.30"),
+                          Td("VSH-MSSQL-SERVER"),
+                          Td("ok"),                      
+                        ),
+                        Tr(
+                          Th("505"),
+                          Td("10.236.5.0/24"),
+                          Td("10.236.5.31"),
+                          Td("VSH-MSSQL-SERVER"),
+                          Td("ok"),                      
+                        ),
+                      ),
+                      cls="striped",
+                      id="ip-tables"
+
+                    )
+                ),  )
           )
 
 
